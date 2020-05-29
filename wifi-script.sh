0d304cf7d7873e66a66e5bc580fc09d6d85e03bc
Robert Kulesza <kuleszamusician@gmail.com>
2020-05-29 13:47:33 -0400

    fixed WIFI_SUCCESS setting error (no spaces)

--- wifi-script.sh
+++ wifi-script.sh
@@ -4,10 +4,9 @@
 # check if wifi exists
 # output success or failure
 
-WIFI_SUCCESS = "Wifi Connection Exists!"
+WIFI_SUCCESS="Wifi Connection Exists!"
 
 if ping -c 1 google.com; then
-	echo "${WIFI_SUCCESS}"
+	echo "$WIFI_SUCCESS"
 	exit
 fi
-
