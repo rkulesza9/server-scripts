4fbd50c57a198b1f0e8ba53bec5b7429b313e78a
Robert Kulesza <kuleszamusician@gmail.com>
2020-05-29 13:57:28 -0400

    added extra lines in variable.

--- wifi-script.sh
+++ wifi-script.sh
@@ -4,9 +4,9 @@
 # check if wifi exists
 # output success or failure
 
-WIFI_SUCCESS="Wifi Connection Exists!"
+WIFI_SUCCESS="\n\nWifi Connection Exists!"
 
 if ping -c 1 google.com; then
-	echo "\n\n$WIFI_SUCCESS"
+	echo "$WIFI_SUCCESS"
 	exit
 fi
