e8c6cf0817ed3b8f404cbad3540fa0a6343dc05c
Robert Kulesza <kuleszamusician@gmail.com>
2020-05-29 14:54:29 -0400

    finished script. not tested.

--- wifi-script.sh
+++ wifi-script.sh
@@ -4,14 +4,46 @@
 # check if wifi exists
 # output success or failure
 
-WIFI_SUCCESS='\n\nWifi Connection Exists!'
+ESSID = "$1"
+PASSWORD = "$2"
+INTERFACE = "$3"
 
-if ping -c 1 google.com; then
-	echo -e "$WIFI_SUCCESS"
-	cd "/$1"
-	pwd
-	exit
-fi
 
-# rfkill unblock wifi
-# iwlist "$1" 
+help(){
+	local HELP = '--help'
+	local HELP_MSG = 'args = [ESSID] [PASSWORD] [INTERFACE]'
+	if [["$ESSID" == "$HELP"]]; then
+		echo "$HELP_MSG"
+	fi
+}
+
+wifi_connected(){
+	local WIFI_SUCCESS='\n\nWifi Connection Exists!'
+	local WIFI_FAILURE='\n\nWifi Connection Failure!'
+
+	if ping -c 1 google.com; then
+		echo -e "$WIFI_SUCCESS"
+	else
+		echo -e "$WIFI_FAILURE"
+	fi
+}
+
+wifi_connect(){
+	local CONFIG = '/etc/wpa_supplicant.conf'
+
+	rfkill unblock wifi
+	killall wpa_supplicant
+
+	wpa_passphrase "$ESSID" "$PASSWORD" > "$CONFIG"
+	wpa_supplicant -c "$CONFIG" -i "$INTERFACE" -B
+
+	dhclient -v "$INTERFACE"
+}
+
+test_method(){
+	local DUCK = "duck"
+	echo "$ESSID $PASSWORD $INTERFACE"
+	echo "$DUCK"
+}
+
+test_method
