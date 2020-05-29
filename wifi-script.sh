b8bd5c90462c53b30ba6eb96d0d60b36276cb9c6
Robert Kulesza <kuleszamusician@gmail.com>
2020-05-29 13:36:23 -0400

    init commit. wifi script.

---
+++ wifi-script.sh
@@ -0,0 +1,13 @@
+# purpose:
+# check if wifi exists
+# if wifi does not exist, then wpa_supplicant operation
+# check if wifi exists
+# output success or failure
+
+WIFI_SUCCESS = "Wifi Connection Exists!"
+
+if ping -c 1 google.com; then
+	echo "${WIFI_SUCCESS}"
+	exit
+fi
+
